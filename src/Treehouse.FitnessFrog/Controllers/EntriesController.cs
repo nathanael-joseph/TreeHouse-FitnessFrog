cabc8a0aebe34bf6cd05392670ad2a493ddc367e
nathanael-joseph <nyebgui66@gmail.com>
2017-07-31 14:51:35 +0300

    enabled client side validation

--- src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
+++ src/Treehouse.FitnessFrog/Controllers/EntriesController.cs
@@ -9,7 +9,7 @@ using Treehouse.FitnessFrog.Models;
 
 namespace Treehouse.FitnessFrog.Controllers
 {
-    public class EntriesController : Controller
+    public class EntriesController : Controller 
     {
         private EntriesRepository _entriesRepository = null;
 
